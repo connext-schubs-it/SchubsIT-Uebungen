0384ce11bf687b132a054d6a273f940984672134
Vogt, Fabian <fabian.vogt@connext.de>
2020-04-06 14:40:09 +0200

    Kommentare eingefügt

--- Console_Uebungen/Aufgabe1a/Program.cs
+++ Console_Uebungen/Aufgabe1a/Program.cs
@@ -10,6 +10,26 @@ namespace Aufgabe1a
    {
       static void Main(string[] args)
       {
+         //Konsolen Titel wird eingetragen
+         Console.Title = "Aufgabe 1a";
+
+         //Vorname wird eingegeben
+         Console.Write("Bitte gib deinen Voramen ein: ");
+         string vorname = Console.ReadLine();
+
+         //Nachname wird eingegeben
+         Console.Write("Bitte gib deinen Nachnamen ein: ");
+         string nachname = Console.ReadLine();
+
+         //Alter wird eingegeben
+         Console.Write("Bitte gib dein Alter ein: ");
+         int alter = Convert.ToInt32(Console.ReadLine());
+
+         //Vorname, Nachname und Alter wird ausgegeben
+         Console.WriteLine("Hallo, " + vorname + " " + nachname + " du bist " + alter + " Jahre alt.");
+
+         //Durch Tastendruck wird Konsolenanwendung beendet
+         Console.ReadKey();
       }
    }
 }
