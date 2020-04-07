637cf5d227f21c79e8309c80510483e5ccbd1efd
Vogt, Fabian <fabian.vogt@connext.de>
2020-04-07 09:33:38 +0200

    aufgabe 2a

---
+++ Console_Uebungen/Aufgabe2a/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe2a
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.Write("Bitte gib Dein Alter ein: ");
+
+         string eingabe = Console.ReadLine();
+         int alter = Convert.ToInt32(eingabe);
+
+         if (alter < 18)
+         {
+            Console.WriteLine("Du bist leider noch nicht erwachsen.");
+         }
+
+         else
+         {
+            Console.WriteLine("Willkommen in der Welt der Erwachsenen!");
+         }
+      }
+   }
+}
